acf0dbb588c371bd18f10b448595154d6ca0d757
vanshAY2 <vansh.gupta_cs23@gla.ac.in>
2026-02-16 11:31:01 +0530

    Node

---
+++ lec5_16feb/file2.js
@@ -0,0 +1,5 @@
+// https://www.amazon.in/AMD-ThreadripperTM-5995WX-128-Thread-Processor/dp/B0B5VLPVL5/ref=sr_1_1?crid=1BTFOLTTMXIEN&dib=eyJ2IjoiMSJ9.vhTIOV2xrvJvdFo1onFygfSEzCBk8SCVd_VzdeDuTnCtHb1CFB8cJKZ9CZ5e8p8fsjbfA9AYGasnBYr5kb3M8NlgVdHdbPqzqhR_QLIpA3-DUxcmB4sxpGQRYorXtdMXdcdt2qMI4Ky2V8krCE9SkyaeoN_LWsSCE9ojp1Jbpxbl4yXfVR9su6A6_9rH18qdqworytPsTXjQYGjfoiY8FtOFLrr-ePgnCC4eQJb9ktY.OlzOKW17Ub2eifgOjzYcq8bxu2AS1_NV4dwQiqf6BY8&dib_tag=se&keywords=ryzen+threadripper+pro+9995wx&qid=1771221184&sprefix=ryzen+thred%2Caps%2C355&sr=8-1
+// https:// -> Protocol
+// www.amazon.in -> Domain Name
+// /AMD-ThreadripperTM-5995WX-128-Thread-Processor -> Path
+// /dp/B0B5VLPVL5/ref=sr_1_1?cri--------2Caps%2C355&sr=8-1 -> Query Parameters
