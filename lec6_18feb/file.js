957e64cfee6a3866d135c1fcd7285e0b40a353f6
vanshAY2 <vansh.gupta_cs23@gla.ac.in>
2026-02-18 14:17:33 +0530

    Node

--- lec6_18feb/file.js
+++ lec6_18feb/file.js
@@ -2,16 +2,22 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     const requestUrl = request.url;
-    // console.log('Request URL:', requestUrl);
     const requestMethod = request.method;
-    // console.log('Request Method:', requestMethod);
-    // response.end('Hello World!');
+// Check if log.txt exists, if not create it
+// Keep on adding the requestUrl to log.txt
+    const fs = require('fs');
+    fs.appendFile("log.txt", requestUrl + "\n", (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
+        }
+    });
 
     if (requestUrl === "/home") {
         response.end("Home Page");
     }
-    if (requestUrl === "/users") {
-        response.end("List of Users");
+    if (requestUrl === "/systemConfig") {
+        const freeMemory = 'Free Memory: ' + (os.freemem() /1024/1024/1024) + ' GB';
+        response.end(freeMemory);
     }
 });
 
